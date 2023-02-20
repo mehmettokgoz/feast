1ffc9ad45f4d72a7eb29096cfaec82d920a59214
mehmettokgoz <mehmet.tokgoz@hazelcast.com>
2023-02-20 18:11:35 +0300

    Replace SQL API with Map API and add TTL support.

--- sdk/python/feast/infra/online_stores/contrib/hazelcast_online_store/hazelcast_online_store.py
+++ sdk/python/feast/infra/online_stores/contrib/hazelcast_online_store/hazelcast_online_store.py
@@ -2,18 +2,17 @@ import logging
 from datetime import datetime
 from typing import Sequence, List, Optional, Tuple, Dict, Callable, Any, Literal
 import pytz
+from hazelcast.core import HazelcastJsonValue
 
 from feast import RepoConfig, FeatureView, Entity
 from feast.infra.key_encoding_utils import serialize_entity_key
 from feast.infra.online_stores.online_store import OnlineStore
 from feast.protos.feast.types.EntityKey_pb2 import EntityKey as EntityKeyProto
-from feast.protos.feast.types.Value_pb2 import Value as ValueProto
+from feast.protos.feast.types.Value_pb2 import Value as ValueProto, Value
 import hazelcast
 
 from feast.repo_config import FeastConfigBaseModel
-
 from pydantic import StrictStr
-from pydantic.types import StrictBool
 
 from feast.usage import log_exceptions_and_usage
 
@@ -30,36 +29,32 @@ class HazelcastOnlineStoreConfig(FeastConfigBaseModel):
     cluster_members: Optional[List] = ["localhost:5701"]
     """List of member addresses which is connected to your cluster"""
 
-    cloud: Optional[StrictBool] = False
-    """Enable if you want to connect a Hazelcast Viridian cluster"""
-
-    discovery_token: Optional[StrictStr]
+    discovery_token: Optional[StrictStr] = ""
     """The discovery token of your Hazelcast Viridian cluster"""
 
-    ssl_enabled: Optional[StrictBool] = False
-    """Enable if you want to use TLS while connecting to your cluster"""
-
-    ssl_cafile_path: Optional[StrictStr]
+    ssl_cafile_path: Optional[StrictStr] = ""
     """Absolute path of concatenated CA certificates used to validate server's certificates in PEM format."""
 
-    ssl_certfile_path: Optional[StrictStr]
+    ssl_certfile_path: Optional[StrictStr] = ""
     """Absolute path of the client certificate in PEM format."""
 
-    ssl_keyfile_path: Optional[StrictStr]
+    ssl_keyfile_path: Optional[StrictStr] = ""
     """Absolute path of the private key file for the client certificate in the PEM format."""
 
-    ssl_password: Optional[StrictStr]
+    ssl_password: Optional[StrictStr] = ""
     """Password for decrypting the keyfile if it is encrypted."""
+
+    key_ttl_seconds: Optional[int] = 0
+    """(Optional) Hazelcast key bin ttl (in seconds) for expiring entities"""
 
 
 class HazelcastOnlineStore(OnlineStore):
     _client: Optional[hazelcast.HazelcastClient] = None
 
     def _get_client(self, config: HazelcastOnlineStoreConfig):
-        logger = logging.getLogger("hazelcast")
-        logger.setLevel(logging.ERROR)
+        logging.getLogger("hazelcast").setLevel(logging.ERROR)
         if not self._client:
-            if config.cloud:
+            if config.discovery_token != "":
                 self._client = hazelcast.HazelcastClient(
                     cluster_name=config.cluster_name,
                     statistics_enabled=True,
@@ -70,7 +65,7 @@ class HazelcastOnlineStore(OnlineStore):
                     ssl_keyfile=config.ssl_keyfile_path,
                     ssl_password=config.ssl_password
                 )
-            elif config.ssl_enabled:
+            elif config.ssl_cafile_path != "":
                 self._client = hazelcast.HazelcastClient(
                     cluster_name=config.cluster_name,
                     statistics_enabled=True,
@@ -98,75 +93,65 @@ class HazelcastOnlineStore(OnlineStore):
             ],
             progress: Optional[Callable[[int], Any]]
     ) -> None:
-
         online_store_config = config.online_store
         assert isinstance(online_store_config, HazelcastOnlineStoreConfig)
 
         client = self._get_client(online_store_config)
-        project = config.project
+        fv_map = client.get_map(_table_id(config.project, table))
 
-        query = f"""
-            SINK INTO {_feature_map_name(project, table)}
-            (entity_key, feature_name, feature_value, event_ts, created_ts)
-            values (?, ?, ?, ?, ?);
-        """
-        for entity_key, features, timestamp, created_ts in data:
+        for entity_key, values, event_ts, created_ts in data:
             entity_key_bin = serialize_entity_key(
                 entity_key,
-                entity_key_serialization_version=2,
+                entity_key_serialization_version=config.entity_key_serialization_version,
             ).hex()
-            timestamp = _convert_to_naive_utc(timestamp)
+            event_ts = _to_utc_timestamp(event_ts)
             if created_ts is not None:
-                created_ts = _convert_to_naive_utc(created_ts)
-            for feature_name, feature_value in features.items():
-                client.sql.execute(query,
-                                   entity_key_bin,
-                                   feature_name,
-                                   feature_value.SerializeToString().hex(),
-                                   timestamp,
-                                   created_ts)
-            if progress:
-                progress(1)
+                created_ts = _to_utc_timestamp(created_ts)
+            for feature_name, feature_value in values.items():
+                feature_value = feature_value.SerializeToString().hex()
+                __key = str(entity_key_bin) + feature_name
+                fv_map.put(__key, HazelcastJsonValue({"feature_name": feature_name,
+                                                      "feature_value": feature_value,
+                                                      "event_ts": event_ts,
+                                                      "created_ts": created_ts}), config.online_store.key_ttl_seconds)
+                if progress:
+                    progress(1)
 
-    @log_exceptions_and_usage(online_store="hazelcast")
     def online_read(
-        self,
-        config: RepoConfig,
-        table: FeatureView,
-        entity_keys: List[EntityKeyProto],
-        requested_features: Optional[List[str]] = None
+            self,
+            config: RepoConfig,
+            table: FeatureView,
+            entity_keys: List[EntityKeyProto],
+            requested_features: Optional[List[str]] = None
     ) -> List[Tuple[Optional[datetime], Optional[Dict[str, ValueProto]]]]:
-
         online_store_config = config.online_store
         assert isinstance(online_store_config, HazelcastOnlineStoreConfig)
 
         client = self._get_client(online_store_config)
 
-        result: List[Tuple[Optional[datetime], Optional[Dict[str, Any]]]] = []
-        requested_features_str = ", ".join(f"'{f}'" for f in requested_features)
-        query = f"""
-                SELECT feature_name, feature_value, event_ts 
-                FROM {_feature_map_name(config.project, table)} 
-                WHERE entity_key=? AND feature_name IN ({requested_features_str})"""
+        entries: List[Tuple[Optional[datetime], Optional[Dict[str, ValueProto]]]] = []
+        fv_map = client.get_map(_table_id(config.project, table))
+
         for entity_key in entity_keys:
-            entity_key_bin = serialize_entity_key(
-                entity_key,
-                entity_key_serialization_version=2,
-            ).hex()
-            records = client.sql.execute(query, entity_key_bin).result()
-            entry = {}
-            entry_ts: Optional[datetime] = None
-            if records:
-                for row in records:
+            entity_key_str = str(serialize_entity_key(entity_key,
+                                                      entity_key_serialization_version=config.entity_key_serialization_version).hex())
+            __keys = []
+            for feature in requested_features:
+                __keys.append(entity_key_str + feature)
+            data = fv_map.get_all(__keys).result()
+            entry = dict()
+            event_ts: Optional[datetime] = None
+            if data:
+                for _key in data:
+                    row = data[_key].loads()
                     value = ValueProto()
                     value.ParseFromString(bytes.fromhex(row["feature_value"]))
                     entry[row["feature_name"]] = value
-                    entry_ts = row["event_ts"]
-            if not entry:
-                result.append((None, None))
+                    event_ts = datetime.utcfromtimestamp(row["event_ts"])
+                entries.append((event_ts, entry))
             else:
-                result.append((entry_ts, entry))
-        return result
+                entries.append((None, None))
+        return entries
 
     def update(
             self,
@@ -185,22 +170,23 @@ class HazelcastOnlineStore(OnlineStore):
 
         for table in tables_to_keep:
             client.sql.execute(
-                f"""CREATE MAPPING IF NOT EXISTS {_feature_map_name(project, table)} (
-                        entity_key VARCHAR EXTERNAL NAME "__key.entity_key",
-                        feature_name VARCHAR EXTERNAL NAME "__key.feature_name",
-                        feature_value OBJECT,
-                        event_ts TIMESTAMP WITH TIME ZONE,
-                        created_ts TIMESTAMP WITH TIME ZONE
+                f"""CREATE OR REPLACE MAPPING {_table_id(project, table)} (
+                        __key VARCHAR,
+                        feature_name VARCHAR,
+                        feature_value VARCHAR,
+                        event_ts INTEGER,
+                        created_ts INTEGER
                     )
                     TYPE IMap
                     OPTIONS (
-                        'keyFormat' = 'json-flat',
+                        'keyFormat' = 'varchar',
                         'valueFormat' = 'json-flat'
                     )
                 """).result()
+
         for table in tables_to_delete:
-            client.sql.execute(f"DELETE FROM {_feature_map_name(config.project, table)}").result()
-            client.sql.execute(f"DROP MAPPING IF EXISTS {_feature_map_name(config.project, table)}").result()
+            client.sql.execute(f"DELETE FROM {_table_id(config.project, table)}").result()
+            client.sql.execute(f"DROP MAPPING IF EXISTS {_table_id(config.project, table)}").result()
 
     def teardown(
             self,
@@ -215,16 +201,16 @@ class HazelcastOnlineStore(OnlineStore):
         project = config.project
 
         for table in tables:
-            client.sql.execute(f"DELETE FROM {_feature_map_name(config.project, table)}")
-            client.sql.execute(f"DROP MAPPING IF EXISTS {_feature_map_name(project, table)}")
+            client.sql.execute(f"DELETE FROM {_table_id(config.project, table)}")
+            client.sql.execute(f"DROP MAPPING IF EXISTS {_table_id(project, table)}")
 
 
-def _feature_map_name(project: str, table: FeatureView) -> str:
+def _table_id(project: str, table: FeatureView) -> str:
     return f"{project}_{table.name}"
 
 
-def _convert_to_naive_utc(ts: datetime) -> datetime:
+def _to_utc_timestamp(ts: datetime) -> int:
     if ts.tzinfo is None:
-        return ts
+        return int(ts.timestamp())
     else:
-        return ts.astimezone(pytz.utc).replace(tzinfo=None)
+        return int(ts.astimezone(pytz.utc).replace(tzinfo=None).timestamp())
